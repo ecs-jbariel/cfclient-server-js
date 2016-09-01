a52b4f78638fdbc33cd924fae021310e4e1b2b56
Jarrett Bariel <jbariel@ecsteam.com>
2016-08-31 23:41:37 -0600

    create example usage for the js-cfclient project

---
+++ index.js
@@ -0,0 +1,32 @@
+O = require('./out');
+
+__IS_TRACE = process.env.DO_TRACE || false;
+__IS_DEBUG = process.env.DO_DEBUG || false;
+
+if (__IS_DEBUG || __IS_TRACE) {
+    O.level(((__IS_TRACE) ? O.LogLevel.TRACE : O.LogLevel.DEBUG));
+}
+
+extend = require("xtend");
+
+connectionParams = require('./paramLoader')();
+O.d("connectionParams: " + JSON.stringify(connectionParams));
+
+const CF = require('js-cfclient');
+
+const CfClient = new CF.CFClient(new CF.CFConfig({
+    protocol: connectionParams.protocol,
+    host: connectionParams.host,
+    username: connectionParams.username,
+    password: connectionParams.password,
+    skipSslValidation: connectionParams.skipSSLValidation
+}));
+
+CfClient.connect().then(() => {
+    CfClient.request('organizations').then((resp) => {
+        O.i('Response: ' + JSON.stringify(resp));
+    }, O.e);
+    CfClient.request('apps').then((resp) => {
+        O.i('Response: ' + JSON.stringify(resp));
+    }, O.e);
+}, O.e);
